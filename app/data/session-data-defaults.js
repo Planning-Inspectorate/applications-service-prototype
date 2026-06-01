3827ea00ab69c25cd0b43cc9d5dbe98f262ef6f8
alexrehopkins <alexrehopkins@gmail.com>
2026-06-01 16:08:38 +0100

    Added change request screen

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -3,7 +3,7 @@ module.exports = {
   // Insert values here
   'redactionscorrect': 'ready',
   'rep_edited': 'unedited',
-  'rep_redacted': 'unredacted',
+  'rep_redacted': 'Unredacted',
   'library-category': 'No examination library category',
   'library-published': 'notpublished',
   'document-status': 'Not checked',
